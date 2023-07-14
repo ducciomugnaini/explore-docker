bac6e645cc47085b1c279e20be9db6e509249315
Duccio Mugnaini <duccio.mugnaini@ascom.com>
2023-07-14 15:19:46 +0200

    update repo

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # explore-docker
 ASP.NET Core applications which can be deployed as Linux containers
 
-[Build ASP.NET Core applications deployed as Linux containers into an AKS/Kubernetes orchestrator](https://learn.microsoft.com/en-us/dotnet/architecture/containerized-lifecycle/design-develop-containerized-apps/build-aspnet-core-applications-linux-containers-aks-kubernetes)
+https://learn.microsoft.com/en-us/dotnet/architecture/containerized-lifecycle/design-develop-containerized-apps/build-aspnet-core-applications-linux-containers-aks-kubernetes
+
+https://github.com/dotnet-architecture/explore-docker
+
+## Debug settings
+
+Debug | Any CPU  
+docker-compose  
+Debug  
+
+WebApp  
+http://localhost:5001/  
+WebApi  
+http://localhost:5000/weatherforecast
+
+## K8s settings
+
+To deploy application
+
+~~~
+kubectl deploy -f deploy-webapi
+kubectl deploy -f deploy-webapp
+~~~
+
+For minikube
+
+~~~
+minikube service webapp
+~~~
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,42 +1,49 @@
-using Microsoft.Extensions.Configuration;
 using WebApp;
 
-var builder = WebApplication.CreateBuilder(args);
-var configuration = GetConfiguration();
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddHttpClient<WeatherClient>(client =>
+internal class Program
 {
-    var baseAddress = new Uri(configuration.GetValue<string>("backendUrl"));
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var configuration = GetConfiguration();
 
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddHttpClient<WeatherClient>(client =>
+        {
+            //var baseAddress = new Uri(configuration.GetValue<string>("backendUrl"));
+            var baseAddress = new Uri(Environment.GetEnvironmentVariable("WebApiBaseAddress") ?? string.Empty);
+            
+            client.BaseAddress = baseAddress;
+        });
 
-    client.BaseAddress = baseAddress;
-});
+        var app = builder.Build();
+        IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
-var app = builder.Build();
-IConfiguration GetConfiguration()
-{
-    var builder = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        .AddEnvironmentVariables();
+            return builder.Build();
+        }
+        
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
 
-    return builder.Build();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
